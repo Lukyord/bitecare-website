c9a9f3b361ee3a654749964681e412a82e574ed4
Lukyord <98599164+Lukyord@users.noreply.github.com>
2023-11-28 17:05:14 +0700

    📦 add images for homepage hero section

--- src/constant/Images.ts
+++ src/constant/Images.ts
@@ -9,6 +9,7 @@
 import ArrowCurveUpWhite from "@public/assets/illustrations/arrow-curve-up-white.svg"
 // B
 import BiteCareLogo from "@public/assets/common/bitecare-logo.svg"
+import BiteCareLogoTextTM from "@public/assets/common/bitecare-logo-text-tm.svg"
 // C
 // D
 import DashedLineBlack from "@public/assets/illustrations/dashed-line-black.svg"
@@ -17,17 +18,25 @@ import EnglishFlag from "@public/assets/common/english-flag.svg"
 // F
 // G
 // H
+import HomepageDog_1 from "@public/assets/homepage/homepage-dog-1.png"
+import HighlightBlack_1 from "@public/assets/illustrations/highlight-1-black.svg"
 // I
 // J
 // K
 // L
+import LowFatFront from "@public/assets/products/low-fat/low-fat-front.png"
 // M
 // N
 // O
 // P
 // Q
 // R
+import RenalCareFront from "@public/assets/products/renal-care/renal-care-front.png"
 // S
+import SparkleBlack_1 from "@public/assets/illustrations/sparkle-1-black.svg"
+import SparkleBlack_2 from "@public/assets/illustrations/sparkle-2-black.svg"
+import SeniorCareFront from "@public/assets/products/senior-care/senior-care-front.png"
+import SkinCareFront from "@public/assets/products/skin-care/skin-care-front.png"
 // T
 import ThaiFlag from "@public/assets/common/thai-flag.svg"
 // V
@@ -41,6 +50,7 @@ export const Images = {
   ArrowCurveUpWhite,
   // B
   BiteCareLogo,
+  BiteCareLogoTextTM,
   // C
   // D
   DashedLineBlack,
@@ -49,17 +59,25 @@ export const Images = {
   // F
   // G
   // H
+  HomepageDog_1,
+  HighlightBlack_1,
   // I
   // J
   // K
   // L
+  LowFatFront,
   // M
   // N
   // O
   // P
   // Q
   // R
+  RenalCareFront,
   // S
+  SparkleBlack_1,
+  SparkleBlack_2,
+  SeniorCareFront,
+  SkinCareFront,
   // T
   ThaiFlag,
   // U
