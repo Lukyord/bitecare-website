690f7bd02367ac2779058f2e6c21ee6f5b5b61fd
Lukyord <98599164+Lukyord@users.noreply.github.com>
2023-12-01 14:06:19 +0700

    📦 imports hompage principle resources

--- src/constant/Images.ts
+++ src/constant/Images.ts
@@ -18,9 +18,14 @@ import DogPawBcInversePrimary from "@public/assets/illustrations/dog-paw-bc-inve
 import EnglishFlag from "@public/assets/common/english-flag.svg"
 // F
 import FacebookLogoFullWhite from "@public/assets/common/facebook-logo-full-white.png"
+import FlowerBcPrimary_1 from "@public/assets/illustrations/flower-bc-primary-1.svg"
+import FlowerBcPrimary_2 from "@public/assets/illustrations/flower-bc-primary-2.svg"
 // G
 // H
+import HashtagBcPrimary from "@public/assets/illustrations/hashtag-bc-primary.svg"
 import HomepageDog_1 from "@public/assets/homepage/homepage-dog-1.png"
+import HomepageDog_2 from "@public/assets/homepage/homepage-dog-2.png"
+import HomepageDog_3_4 from "@public/assets/homepage/homepage-dog-3-4.png"
 import HighlightBlack_1 from "@public/assets/illustrations/highlight-1-black.svg"
 // I
 // J
@@ -39,6 +44,9 @@ import RenalCareFront from "@public/assets/products/renal-care/renal-care-front.
 import ShopeeLogoFullWhite from "@public/assets/common/shopee-logo-full-white.png"
 import SparkleBlack_1 from "@public/assets/illustrations/sparkle-1-black.svg"
 import SparkleBlack_2 from "@public/assets/illustrations/sparkle-2-black.svg"
+import SparkleBcInversePrimary_1 from "@public/assets/illustrations/sparkle-bc-inverse-primary-1.svg"
+import SparkleBcInversePrimary_2 from "@public/assets/illustrations/sparkle-bc-inverse-primary-2.svg"
+import SparkleBcInversePrimary_3 from "@public/assets/illustrations/sparkle-bc-inverse-primary-3.svg"
 import SeniorCareFront from "@public/assets/products/senior-care/senior-care-front.png"
 import SkinCareFront from "@public/assets/products/skin-care/skin-care-front.png"
 // T
@@ -65,9 +73,14 @@ export const Images = {
   EnglishFlag,
   // F
   FacebookLogoFullWhite,
+  FlowerBcPrimary_1,
+  FlowerBcPrimary_2,
   // G
   // H
+  HashtagBcPrimary,
   HomepageDog_1,
+  HomepageDog_2,
+  HomepageDog_3_4,
   HighlightBlack_1,
   // I
   // J
@@ -86,6 +99,9 @@ export const Images = {
   ShopeeLogoFullWhite,
   SparkleBlack_1,
   SparkleBlack_2,
+  SparkleBcInversePrimary_1,
+  SparkleBcInversePrimary_2,
+  SparkleBcInversePrimary_3,
   SeniorCareFront,
   SkinCareFront,
   // T
