094e78cb7634c67fa45fecdc5a3e3e5adcc265bc
nontharatbang <nontharatbang@gmail.com>
2024-01-04 13:47:20 +0700

    🧑‍💻 Integrate useNavigation to the FooterNavigateSection component

--- src/components/common/Footer/FooterNavigateSection.tsx
+++ src/components/common/Footer/FooterNavigateSection.tsx
@@ -2,68 +2,15 @@ import { getTranslations } from "next-intl/server"
 import Image from "next/image"
 import Link from "next/link"
 import { Images } from "@/constant/Images"
+import useNavigation from "@/hooks/useNavigation"
 
 export default async function FooterNavigateSection() {
-  const tFooter = await getTranslations("footer")
-  const tFooterProduct = await getTranslations("footer-product")
-  const tFooterSupport = await getTranslations("footer-support")
-  const tFooterWhereToBuy = await getTranslations("footer-where-to-buy")
-
-  const productList = [
-    {
-      label: tFooterProduct("skin-care"),
-      href: "/products/skin-care",
-    },
-    {
-      label: tFooterProduct("low-fat"),
-      href: "/products/low-fat",
-    },
-    {
-      label: tFooterProduct("senior-care"),
-      href: "/products/senior-care",
-    },
-    {
-      label: tFooterProduct("renal-care"),
-      href: "/products/renal-care",
-    },
-  ]
-
-  const supportList = [
-    {
-      label: tFooterSupport("contact-us"),
-      href: "/support/contact-us",
-    },
-    {
-      label: tFooterSupport("faq"),
-      href: "/support/faq",
-    },
-  ]
-
-  const whereToBuyList = [
-    {
-      label: tFooterWhereToBuy("physical-store"),
-      href: "/where-to-buy?type=physical-store",
-    },
-    {
-      label: tFooterWhereToBuy("online-platform"),
-      href: "/where-to-buy?type=online-platform",
-    },
-  ]
-
-  const footerItems = [
-    {
-      label: tFooter("products"),
-      list: productList,
-    },
-    {
-      label: tFooter("support"),
-      list: supportList,
-    },
-    {
-      label: tFooter("where-to-buy"),
-      list: whereToBuyList,
-    },
-  ]
+  const NavigationMenus = await useNavigation()
+  
+  const footerItems = NavigationMenus.map((menu) => ({
+    label: menu.label,
+    breadCrumbs: menu.breadCrumbs,
+  }))
 
   return (
     <div className="mx-auto flex w-[90%] flex-col items-center justify-between pt-[6%] md:flex-row">
@@ -79,12 +26,12 @@ export default async function FooterNavigateSection() {
             key={index}
           >
             <span className="pb-4">{footerItem.label}</span>
-            {footerItem.list.map((item, index) => (
+            {footerItem.breadCrumbs.map((breadCrumb, index) => (
               <li
                 className="pt-4 text-left text-subtitle text-neutral-500 lg:text-paragraph"
                 key={index}
               >
-                <Link href={item.href}>{item.label}</Link>
+                <Link href={breadCrumb.href}>{breadCrumb.title}</Link>
               </li>
             ))}
           </ul>
