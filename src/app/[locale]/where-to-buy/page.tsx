75497e9a745fcab13663319ea851f0906a671609
nontharatbang <nontharatbang@gmail.com>
2024-01-10 02:02:39 +0700

    🍕 Draft where to buy landing page

--- src/app/[locale]/where-to-buy/page.tsx
+++ src/app/[locale]/where-to-buy/page.tsx
@@ -1,9 +1,14 @@
 import { Metadata } from "next"
 import { pick } from "lodash"
 import { NextIntlClientProvider } from "next-intl"
-import { getMessages, unstable_setRequestLocale } from "next-intl/server"
+import {
+  getMessages,
+  getTranslations,
+  unstable_setRequestLocale,
+} from "next-intl/server"
 
 import PhysicalStores from "@/components/where-to-buy/PhysicalStores/PhysicalStores"
+import OnlineStores from "@/components/where-to-buy/OnlineStores/OnlineStores"
 
 export const metadata: Metadata = {
   title: "Where To Buy",
@@ -17,6 +22,7 @@ export default async function WhereToBuyPage({
   unstable_setRequestLocale(locale)
 
   const messages = await getMessages()
+  const tWhereToBuy = await getTranslations("where-to-buy-landing")
 
   return (
     <div className="pt-36">
@@ -25,9 +31,17 @@ export default async function WhereToBuyPage({
           messages,
           "physical-store",
           "physical-store-toast",
+          "online-store",
           "button"
         )}
       >
+        <div>
+          <h1 className="my-8 text-center text-h3 lg:text-h2 2xl:my-20 2xl:text-h1">
+            {tWhereToBuy("where-to-get-bite-care")}
+          </h1>
+          {/* TODO: Component for change tabs */}
+        </div>
+        <OnlineStores />
         <PhysicalStores />
       </NextIntlClientProvider>
     </div>
