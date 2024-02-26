6800e685a816c343356f1a45669d6fb03c66ab54
Kongtup <ttansiri524@gmail.com>
2024-02-26 18:39:49 +0700

    🐛 fix url bug

--- src/components/where-to-buy/WhereToBuyTabs.tsx
+++ src/components/where-to-buy/WhereToBuyTabs.tsx
@@ -3,7 +3,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import PhysicalStores from "@/components/where-to-buy/PhysicalStores/PhysicalStores"
 import OnlineStores from "@/components/where-to-buy/OnlineStores/OnlineStores"
-import { useRouter, useSearchParams } from "next/navigation"
+import { useSearchParams } from "next/navigation"
 import { useTranslations } from "next-intl"
 
 const tabs = {
@@ -12,16 +12,9 @@ const tabs = {
 }
 
 export default function WhereToBuyTabs() {
-  const router = useRouter()
   const searchParams = useSearchParams()
   const tWhereToBuy = useTranslations("where-to-buy-landing")
-  const defaultTab = searchParams.get("type") || "physical-store"
-
-  const handleTabChange = () => {
-    if (defaultTab === tabs.physicalStore) {
-      router.replace(`?type=${tabs.onlinePlatform}`, { scroll: false })
-    } else router.replace(`?type=${tabs.physicalStore}`, { scroll: false })
-  }
+  const defaultTab = searchParams.get("type") || tabs.physicalStore
 
   return (
     <Tabs
@@ -29,25 +22,25 @@ export default function WhereToBuyTabs() {
       defaultValue={defaultTab}
       className="flex flex-col items-center"
     >
-      <TabsList className="mb-10 grid w-[80%] max-w-md grid-cols-2">
-        <TabsTrigger value="physical-store" onClick={() => handleTabChange()}>
+      <TabsList className="mb-10 grid w-[80%] max-w-md grid-cols-2 ">
+        <TabsTrigger value={tabs.physicalStore}>
           {tWhereToBuy("physical-store")}
         </TabsTrigger>
-        <TabsTrigger value="online-platform" onClick={() => handleTabChange()}>
+        <TabsTrigger value="online-platform">
           {tWhereToBuy("online-platform")}
         </TabsTrigger>
       </TabsList>
 
       <TabsContent
-        value="physical-store"
-        className="sub-desktop:my-20 my-10 w-full"
+        value={tabs.physicalStore}
+        className="my-10 w-full sub-desktop:my-20"
       >
         <PhysicalStores />
       </TabsContent>
 
       <TabsContent
-        value="online-platform"
-        className="sub-desktop:my-20 my-10 w-full"
+        value={tabs.onlinePlatform}
+        className="my-10 w-full sub-desktop:my-20"
       >
         <OnlineStores />
       </TabsContent>
