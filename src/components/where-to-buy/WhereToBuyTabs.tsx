8314614d4368a1232ee1577870a64db542de566b
Kongtup <ttansiri524@gmail.com>
2024-02-26 18:58:15 +0700

    🐛 fix default tab bug

--- src/components/where-to-buy/WhereToBuyTabs.tsx
+++ src/components/where-to-buy/WhereToBuyTabs.tsx
@@ -3,7 +3,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import PhysicalStores from "@/components/where-to-buy/PhysicalStores/PhysicalStores"
 import OnlineStores from "@/components/where-to-buy/OnlineStores/OnlineStores"
-import { useSearchParams } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
 import { useTranslations } from "next-intl"
 
 const tabs = {
@@ -12,21 +12,27 @@ const tabs = {
 }
 
 export default function WhereToBuyTabs() {
+  const router = useRouter()
   const searchParams = useSearchParams()
   const tWhereToBuy = useTranslations("where-to-buy-landing")
-  const defaultTab = searchParams.get("type") || tabs.physicalStore
+  const defaultTab = searchParams.get("type") || "physical-store"
+
+  const handleTabChange = () => {
+    if (defaultTab === tabs.physicalStore) {
+      router.replace(`?type=${tabs.onlinePlatform}`, { scroll: false })
+    } else router.replace(`?type=${tabs.physicalStore}`, { scroll: false })
+  }
 
   return (
-    <Tabs
-      key={defaultTab}
-      defaultValue={defaultTab}
-      className="flex flex-col items-center"
-    >
-      <TabsList className="mb-10 grid w-[80%] max-w-md grid-cols-2 ">
-        <TabsTrigger value={tabs.physicalStore}>
+    <Tabs defaultValue={defaultTab} className="flex flex-col items-center">
+      <TabsList className="mb-10 grid w-[80%] max-w-md grid-cols-2">
+        <TabsTrigger
+          value={tabs.physicalStore}
+          onClick={() => handleTabChange()}
+        >
           {tWhereToBuy("physical-store")}
         </TabsTrigger>
-        <TabsTrigger value="online-platform">
+        <TabsTrigger value="online-platform" onClick={() => handleTabChange()}>
           {tWhereToBuy("online-platform")}
         </TabsTrigger>
       </TabsList>
