33fe2226e4f400f9fe6aa2dc4afe3c0df86febe6
nkaewam <tontan2545@gmail.com>
2024-12-24 17:37:30 +0700

    fix: remove homepage out of i18n json

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,10 @@
 type CommonMessages = typeof import("./public/locales/en/common.json")
-type HomePageMessages = typeof import("./public/locales/en/homepage.json")
 type ProductsMessages = typeof import("./public/locales/en/products.json")
 type ProductMessages = typeof import("./public/locales/en/product.json")
 type WhereToBuyMessages = typeof import("./public/locales/en/where-to-buy.json")
 type AboutUsMessages = typeof import("./public/locales/en/about-us.json")
 
-type MergedMessages = HomePageMessages &
-  CommonMessages &
+type MergedMessages = CommonMessages &
   ProductsMessages &
   ProductMessages &
   WhereToBuyMessages &
